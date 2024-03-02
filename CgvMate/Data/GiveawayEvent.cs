919fe7258b4319f2e47c724faf9b53e5a0cd1c58
WooRim Jeon <99327851+woorim02@users.noreply.github.com>
2024-03-02 23:40:29 +0900

    disable CS8618

--- CgvMate/Data/GiveawayEvent.cs
+++ CgvMate/Data/GiveawayEvent.cs
@@ -1,5 +1,6 @@
 ﻿namespace CgvMate.Data;
 
+#pragma warning disable CS8618 // 생성자를 종료할 때 null을 허용하지 않는 필드에 null이 아닌 값을 포함해야 합니다. null 허용으로 선언해 보세요.
 public class GiveawayEvent
 {
     public string Title { get; init; }
--- CgvMate/Data/GiveawayEventModel.cs
+++ CgvMate/Data/GiveawayEventModel.cs
@@ -1,10 +1,11 @@
 ﻿namespace CgvMate.Data;
 
+#pragma warning disable CS8618 // 생성자를 종료할 때 null을 허용하지 않는 필드에 null이 아닌 값을 포함해야 합니다. null 허용으로 선언해 보세요.
 public class GiveawayEventModel
 {
-    public string Title { get => GiveawayItemList?[0].GiveawayItemName; }
+    public string Title { get => GiveawayItemList[0].GiveawayItemName; }
     public string EventIndex { get; init; }
-    public string GiveawayIndex { get => GiveawayItemList?[0].GiveawayItemCode; }
+    public string GiveawayIndex { get => GiveawayItemList[0].GiveawayItemCode; }
     public string Contents { get; init; }
 
     /// <summary>
