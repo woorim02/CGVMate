8231847ab13b769e7810e044797c082e18717aa0
WooRim Jeon <99327851+woorim02@users.noreply.github.com>
2024-03-06 21:06:44 +0900

    GetScheduleListAsync 구조 변경, 오버로딩 함수 추가

--- CgvMate/Service/CgvReservationService.cs
+++ CgvMate/Service/CgvReservationService.cs
@@ -19,8 +19,19 @@ public class CgvReservationService : CgvServiceBase
         _authHandler = authHandler;
     }
 
-    public async Task<TheaterScheduleListRoot> GetScheduleListAsync(string theaterCode, string? movieGroupCd, DateOnly date, string screenTypeCode = "02")
+    public async Task<TheaterScheduleListRoot> GetScheduleListAsync(string theaterCode, string? movieGroupCd, DateOnly date, string movieTypeCd = "00")
+        => await GetScheduleListAsync([theaterCode], [movieGroupCd is null ? string.Empty : movieTypeCd], date, movieTypeCd);
+
+    public async Task<TheaterScheduleListRoot> GetScheduleListAsync(string[] theaterCodes, string[] movieGroupCds, DateOnly date, string movieTypeCd = "00")
     {
+        foreach (var item in theaterCodes)
+            ArgumentException.ThrowIfNullOrEmpty(item, nameof(theaterCodes));
+        if(theaterCodes.Length > 2)
+            throw new ArgumentOutOfRangeException(nameof(theaterCodes), theaterCodes, "한번에 검색할 수 있는 극장 수는 2개 이하입니다.");
+        if (movieGroupCds.Length > 3)
+            throw new ArgumentOutOfRangeException(nameof(movieGroupCds), movieGroupCds, "한번에 검색할 수 있는 영화 수는 3개 이하입니다.");
+
+
         var request = new HttpRequestMessage(HttpMethod.Get, "https://m.cgv.co.kr/WebApp/Reservation/Schedule.aspx");
         await _client.SendAsync(request);
 
@@ -31,22 +42,22 @@ public class CgvReservationService : CgvServiceBase
         request.Headers.Add("Cookie", "URL_PREV_COMMON=");
         var body = new
         {
-            strRequestType = "THEATER",
+            strRequestType = "COMPARE",
             strUserID = "",
-            strMovieGroupCd = movieGroupCd,
-            strMovieTypeCd = "",
+            strMovieGroupCd = string.Join('|', movieGroupCds),
+            strMovieTypeCd = movieTypeCd,
             strPlayYMD = $"{date:yyyyMMdd}",
-            strTheaterCd = theaterCode,
-            strScreenTypeCd = screenTypeCode,
+            strTheaterCd = string.Join('|', theaterCodes),
+            strScreenTypeCd = "00",
             strRankType = "MOVIE",
         };
         request.Content = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
 
         var response = await _client.SendAsync(request);
         var content = await response.Content.ReadAsStringAsync();
-        content = JObject.Parse(content)["d"]?.ToString();
+        content = JObject.Parse(content)["d"]!.ToString();
         var root = JsonConvert.DeserializeObject<TheaterScheduleListRoot>(content);
-        return root;
+        return root!;
     }
 
     public async Task<MiniMapDataRoot> GetMiniMapDataAsync(Schedule schedule)
@@ -72,8 +83,8 @@ public class CgvReservationService : CgvServiceBase
         request.Content = new StringContent(JsonConvert.SerializeObject(json), Encoding.UTF8, "application/json");
         var response = await _client.SendAsync(request);
         var content = await response.Content.ReadAsStringAsync();
-        content = JObject.Parse(content)["d"].ToString();
+        content = JObject.Parse(content)["d"]!.ToString();
         var root = JsonConvert.DeserializeObject<MiniMapDataRoot>(content);
-        return root;
+        return root!;
     }
 }
