2a4c9e05af6472abf3f485ae54ed8a83080b0865
WooRim Jeon <99327851+woorim02@users.noreply.github.com>
2024-03-09 23:23:17 +0900

    필요 없는 필드 삭제

--- CgvMate/Core/AppDbContext.cs
+++ CgvMate/Core/AppDbContext.cs
@@ -2,47 +2,45 @@
 
 namespace CgvMate.Core
 {
-	public class AppDbContext : DbContext
-	{
-		public DbSet<Area> Areas { get; set; }
-		public DbSet<Theater> Theaters{ get; set; }
-		public DbSet<Movie> Movies { get; set; }
-		public DbSet<TheaterGiveawayInfo> TheaterGiveawayInfos { get; set; }
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Area> Areas { get; set; }
+        public DbSet<Theater> Theaters{ get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<OpenNotificationInfo> OpenNotificationInfos { get; set; }
 
-		public DbSet<OpenNotificationInfo> OpenNotificationInfos { get; set; }
+        public AppDbContext()
+        {
 
-		public AppDbContext()
-		{
+        }
 
-		}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename={Constants.APP_DB_PATH}");
+        }
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		{
-			optionsBuilder.UseSqlite($"Filename={Constants.APP_DB_PATH}");
-		}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Area>()
+                .HasKey(x => x.AreaCode);
 
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			modelBuilder.Entity<Area>()
-				.HasKey(x => x.AreaCode);
+            modelBuilder.Entity<Theater>()
+                .HasKey(x=>x.TheaterCode);
 
-			modelBuilder.Entity<Theater>()
-				.HasKey(x=>x.TheaterCode);
+            modelBuilder.Entity<Movie>()
+                .HasKey(x => x.Index);
+            modelBuilder.Entity<Movie>()
+                .Ignore(x=>x.ScreenTypes);
 
-			modelBuilder.Entity<Movie>()
-				.HasKey(x => x.Index);
-			modelBuilder.Entity<Movie>()
-				.Ignore(x=>x.ScreenTypes);
-
-			modelBuilder.Entity<OpenNotificationInfo>()
-				.HasKey(x => x.Id);
-			modelBuilder.Entity<OpenNotificationInfo>()
-				.Property(x => x.Id)
-				.ValueGeneratedOnAdd();
-			modelBuilder.Entity<OpenNotificationInfo>()
-				.HasOne(x => x.Movie);
-			modelBuilder.Entity<OpenNotificationInfo>()
-				.HasOne(x => x.Theater);
-		}
-	}
+            modelBuilder.Entity<OpenNotificationInfo>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<OpenNotificationInfo>()
+                .Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<OpenNotificationInfo>()
+                .HasOne(x => x.Movie);
+            modelBuilder.Entity<OpenNotificationInfo>()
+                .HasOne(x => x.Theater);
+        }
+    }
 }
