6d81bf0a0bf56fd3edcfebe68266f822f3d350c7
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-06-20 14:18:04 +0900

    CgvMate.App 프로젝트 추가

---
+++ CgvMate.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CgvMate.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
