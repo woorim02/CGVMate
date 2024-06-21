e090f1a3cc88c749dc848b7ced306dcda75d1a6a
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-06-21 12:38:54 +0900

    마이그레이션 추가

---
+++ CgvMate.Api/Migrations/20240621033337_AddLotteGiveawayKeywords.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CgvMate.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLotteGiveawayKeywords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LotteGiveawayEventKeywords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Keyword = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LotteGiveawayEventKeywords", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LotteGiveawayEventKeywords_Keyword",
+                table: "LotteGiveawayEventKeywords",
+                column: "Keyword",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LotteGiveawayEventKeywords");
+        }
+    }
+}
