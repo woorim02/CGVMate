407177bdae83e0e9f7118976554e7375a1060654
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-06-29 22:14:48 +0900

    Model에 HasNext 추가

--- CgvMate.Data/Entities/LotteCinema/LotteGiveawayEventModel.cs
+++ CgvMate.Data/Entities/LotteCinema/LotteGiveawayEventModel.cs
@@ -5,4 +5,8 @@ public class LotteGiveawayEventModel
     public string EventID { get; set; }
     public string FrGiftID { get; set; }
     public string FrGiftNm { get; set; }
+    /// <summary>
+    /// 기본값 = <see langword="false"/>
+    /// </summary>
+    public bool HasNext { get; set; } = false;
 }
