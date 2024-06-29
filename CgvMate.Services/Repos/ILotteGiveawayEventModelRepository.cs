46793c0b9e545a37cdff557a33baabd4e9ad1a3b
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-06-29 23:04:19 +0900

    ILotteGiveawayEventModelRepository 추가

---
+++ CgvMate.Services/Repos/ILotteGiveawayEventModelRepository.cs
@@ -0,0 +1,10 @@
+﻿using CgvMate.Data.Entities.LotteCinema;
+
+namespace CgvMate.Services.Repos;
+
+public interface ILotteGiveawayEventModelRepository
+{
+    Task<IEnumerable<LotteGiveawayEventModel>> GetAllAsync();
+    Task<LotteGiveawayEventModel?> GetAsync(string EventID);
+    Task AddAsync(LotteGiveawayEventModel model);
+}
