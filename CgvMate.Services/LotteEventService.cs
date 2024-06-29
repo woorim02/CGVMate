cd99ebd6efb3d7915f63009335effaa2a7b98765
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-06-29 23:05:14 +0900

    하나의 이벤트에 두개 이상의 경품 이벤트가 존재하는 상황 추가

--- CgvMate.Services/LotteEventService.cs
+++ CgvMate.Services/LotteEventService.cs
@@ -12,12 +12,17 @@ public class LotteService
     private readonly HttpClient _client;
     private readonly ILotteGiveawayEventRepository _giveawayEventRepository;
     private readonly ILotteGiveawayEventKeywordRepository _giveawayEventKeywordRepository;
+    private readonly ILotteGiveawayEventModelRepository _giveawayEventModelRepository;
 
-    public LotteService(HttpClient client, ILotteGiveawayEventRepository giveawayEventRepository, ILotteGiveawayEventKeywordRepository giveawayEventKeywordRepository)
+    public LotteService(HttpClient client,
+                        ILotteGiveawayEventRepository giveawayEventRepository,
+                        ILotteGiveawayEventKeywordRepository giveawayEventKeywordRepository,
+                        ILotteGiveawayEventModelRepository giveawayEventModelRepository)
     {
         _client = client;
         _giveawayEventRepository = giveawayEventRepository;
         _giveawayEventKeywordRepository = giveawayEventKeywordRepository;
+        _giveawayEventModelRepository = giveawayEventModelRepository;
     }
 
     public async Task<List<Event>> GetEventsAsync(LotteEventType type)
@@ -51,27 +56,69 @@ public class LotteService
 
     public async Task<List<Event>> GetGiveawayEventsAsync()
     {
+        // 영화 이벤트 목록 가져오기
         var events = await GetEventsAsync(LotteEventType.영화);
+        // 필터링할 키워드 가져오기
         var keywords = await _giveawayEventKeywordRepository.GetAllKeywordsAsync();
+        // 필터링된 이벤트 리스트 생성
         var keywordSet = new HashSet<string>(keywords);
-
-        var list = events
+        IReadOnlyList<Event> list = events
             .Where(e => keywordSet.Any(k => e.EventName.Contains(k)))
             .ToList();
-
-        return list;
+        // 데이터베이스에 저장된 모델 목록 불러오기
+        var eventModels = (await _giveawayEventModelRepository.GetAllAsync()).ToDictionary( x => x.EventID, x => x);
+        var finalList = new List<Event>();
+        foreach(var @event in list) 
+        {
+            finalList.Add(@event);
+            // 아직 모델이 조회된 적 없으면 건너뛰기
+            if (!eventModels.ContainsKey(@event.EventID))
+                continue;
+            // 다음 모델이 없으면 건너뛰기
+            var model = eventModels[@event.EventID];
+            if (!model.HasNext)
+                continue;
+            // list(서버에서 가져온 값)에 다음 이벤트 아이디가 존재하면 건너뛰기
+            var nextID = (long.Parse(@event.EventID) + 1).ToString();
+            var count = list.Where(e => e.EventID == nextID).Count();
+            if (count > 0)
+                continue;
+            // 다음 모델 불러오기
+            var nextModel = await GetLotteGiveawayEventModelAsync(nextID);
+            // 다음 이벤트를 final리스트에 추가
+            Event nextEvent = DeepCopy(@event);
+            nextEvent.EventID = nextID;
+            nextEvent.EventName = nextModel.FrGiftNm;
+            finalList.Add(nextEvent);
+        }
+        return finalList;
     }
 
-    public async Task<LotteGiveawayEventModel> GetLotteGiveawayEventModelAsync(string eventID)
+    public async Task<LotteGiveawayEventModel?> GetLotteGiveawayEventModelAsync(string eventID)
     {
+        // 데이터베이스에서 모델 정보 가져오기
+        var model = await _giveawayEventModelRepository.GetAsync(eventID);
+        if (model != null)
+            return model;// 존재하면 리턴
+
+        //존재하지 않으면 서버에서 불러오기
         var request = new HttpRequestMessage(HttpMethod.Post, "https://event.lottecinema.co.kr/LCWS/Event/EventData.aspx");
         var body = new InfomationDeliveryEventDetailReqDTO(eventID);
         var json = await SendForm(request, JsonConvert.SerializeObject(body));
         var root = JsonConvert.DeserializeObject<InfomationDeliveryEventDetailResDTO>(json);
         if (root.IsOK != "true")
             return null;
+        if (root.InfomationDeliveryEventDetail.Count == 0)
+            return null;
+        model = root.InfomationDeliveryEventDetail[0].GoodsGiftItems[0];
+        // 조회수 올리기
         await _giveawayEventRepository.UpdateViewAsync(eventID);
-        return root.InfomationDeliveryEventDetail[0].GoodsGiftItems[0];
+        // 다음 이벤트 존재하는지 확인
+        var nextModel = await GetLotteGiveawayEventModelAsync((long.Parse(eventID) + 1).ToString());
+        model.HasNext = nextModel is not null;
+        // 모델 저장
+        await _giveawayEventModelRepository.AddAsync(model);
+        return model;
     }
 
     public async Task<GiveawayEventDetail> GetLotteGiveawayInfoAsync(string eventID, string giftID)
@@ -99,5 +146,11 @@ public class LotteService
         var response = await _client.SendAsync(request);
         var json = await response.Content.ReadAsStringAsync();
         return json;
+    }
+
+    public static T DeepCopy<T>(T obj)
+    {
+        string serialized = JsonConvert.SerializeObject(obj);
+        return JsonConvert.DeserializeObject<T>(serialized);
     }
 }
