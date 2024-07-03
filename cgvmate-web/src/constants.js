1180584a0d7a6f37fbe60ea858f06f49e12e625e
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-07-04 01:01:05 +0900

    constants 추가

---
+++ cgvmate-web/src/constants.js
@@ -0,0 +1,11 @@
+// Constants 정의
+const Constants = {
+    event_: "/event",
+    event_giveaway: "/event/giveaway",
+    event_cupon_speed: "/event/cupon/speed",
+    event_cupon_surprise: "/event/cupon/surprise",
+    lotte_event: "/lotte/event",
+    lotte_event_giveaway: "/lotte/event/giveaway",
+    megabox_event_giveaway: "/megabox/event/giveaway",
+  };
+export default Constants;
