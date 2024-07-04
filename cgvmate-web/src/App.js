d366cc91d650d3e49e077f7b43e7fbf08f92591d
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-07-04 10:28:52 +0900

    라우터 추가 - 홈페이지, notfound

--- cgvmate-web/src/App.js
+++ cgvmate-web/src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
 import Offcanvas from './components/Offcanvas/Offcanvas';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -18,6 +22,12 @@ function App() {
           <Sidebar className="sidebar" isOpen={true} />
         </div>
         <main>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          </BrowserRouter>
         </main>
       </div>
     </div>
