ca2666c77286e7c3c8255419350aaa0bd50072f4
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-07-04 10:50:59 +0900

    constatnst 추가

--- cgvmate-web/src/constants.js
+++ cgvmate-web/src/constants.js
@@ -1,11 +1,24 @@
-// Constants 정의
 const Constants = {
-    event_: "/event",
-    event_giveaway: "/event/giveaway",
-    event_cupon_speed: "/event/cupon/speed",
-    event_cupon_surprise: "/event/cupon/surprise",
-    lotte_event: "/lotte/event",
-    lotte_event_giveaway: "/lotte/event/giveaway",
-    megabox_event_giveaway: "/megabox/event/giveaway",
-  };
+  // Routes
+  root: "/",
+
+  event_: "/cgv/event",
+  event_cupon_speed: "/cgv/event/cupon/speed",
+  event_cupon_surprise: "/cgv/event/cupon/surprise",
+  event_giveaway: "/cgv/event/giveaway",
+  event_giveaway_detail: "/cgv/event/giveaway/detail",
+  event_giveaway_autosignup: "/cgv/event/giveaway/autosignup",
+
+  lotte_event: "/lotte/event",
+  lotte_event_giveaway: "/lotte/event/giveaway",
+  lotte_event_giveaway_detail: "/lotte/event/giveaway/detail",
+
+  megabox_event_giveaway: "/megabox/event/giveaway",
+  megabox_event_giveaway_detail: "/megabox/event/giveaway/detail",
+
+  // API Host
+  API_HOST: "https://api.cgvmate.com"
+};
+
+// Export the constants for use in other modules
 export default Constants;
