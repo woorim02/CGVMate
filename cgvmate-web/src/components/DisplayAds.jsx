5652668330b59d984e4e5c29548b768b33777d2f
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-07-04 23:45:50 +0900

    디스플레이 광고 추가

---
+++ cgvmate-web/src/components/DisplayAds.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const DisplayAds = ({ adSlot, style }) => {
+  useEffect(() => {
+    const pushAd = () => {
+      try {
+        const adsbygoogle = window.adsbygoogle;
+        adsbygoogle.push({});
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    let interval = setInterval(() => {
+      // Check if Adsense script is loaded every 300ms
+      if (window.adsbygoogle) {
+        pushAd();
+        // clear the interval once the ad is pushed so that function isn't called indefinitely
+        clearInterval(interval);
+      }
+    }, 300);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{style}}
+      data-ad-client="ca-pub-2422895337222657"
+      data-ad-slot={adSlot}
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+  );
+};
+
+DisplayAds.propTypes = {
+  adSlot: PropTypes.string.isRequired,
+  style: PropTypes.string,
+};
+
+DisplayAds.defaultProps = {
+  style: "display: 'block'",
+};
+
+export default DisplayAds;
