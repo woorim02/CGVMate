6714e3220a94039ce0fb16c9836975e83d9ef9da
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-07-08 00:33:04 +0900

    메가박스 이벤트 클래스 추가

---
+++ CgvMate.Data/Entities/Megabox/Event.cs
@@ -0,0 +1,9 @@
+﻿namespace CgvMate.Data.Entities.Megabox;
+
+public class Event
+{
+    public string Title { get; set; }
+    public string Date { get; set; }
+    public string ImageUrl { get; set; }
+    public string Link { get; set; }
+}
