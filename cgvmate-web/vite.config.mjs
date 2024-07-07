351a43f4f1360f033cf39b5b1a542e9b29fe8c4b
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-07-07 22:45:32 +0900

    remove Identifier, 몇가지 수정

--- cgvmate-web/vite.config.mjs
+++ cgvmate-web/vite.config.mjs
@@ -1,46 +1,40 @@
-import { defineConfig, loadEnv } from 'vite';
+// https://github.com/vitejs/vite/discussions/3448
+// import path from 'path';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
-import path from 'path';
 
 // ----------------------------------------------------------------------
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables from `envFile`
-  const env = loadEnv(mode, process.cwd(), 'process.env');
-
-  return {
-    plugins: [react(), jsconfigPaths()],
-    base: '',
-    define: {
-      global: 'window',
-      'process.env': env,  // Pass the loaded environment variables
-    },
-    resolve: {
-      // alias: [
-      //   {
-      //     find: /^~(.+)/,
-      //     replacement: path.join(process.cwd(), 'node_modules/$1')
-      //   },
-      //   {
-      //     find: /^src(.+)/,
-      //     replacement: path.join(process.cwd(), 'src/$1')
-      //   }
-      // ]
-    },
-    build: {
-      outDir: 'build', // 출력 디렉터리를 'build'로 변경
-      rollupOptions: {
-        external: ['react-helmet-async'], // 외부 모듈로 설정하여 Rollup 경고 해결
-      },
-    },
-    server: {
-      open: true,
-      port: 3000
-    },
-    preview: {
-      open: true,
-      port: 3000
-    }
-  };
+export default defineConfig({
+  plugins: [react(), jsconfigPaths()],
+  // https://github.com/jpuri/react-draft-wysiwyg/issues/1317
+  base: '/',
+  define: {
+    global: 'window'
+  },
+  resolve: {
+    // alias: [
+    //   {
+    //     find: /^~(.+)/,
+    //     replacement: path.join(process.cwd(), 'node_modules/$1')
+    //   },
+    //   {
+    //     find: /^src(.+)/,
+    //     replacement: path.join(process.cwd(), 'src/$1')
+    //   }
+    // ]
+  },
+  server: {
+    // this ensures that the browser opens upon server start
+    open: true,
+    // this sets a default port to 3000
+    port: 3000
+  },
+  preview: {
+    // this ensures that the browser opens upon preview start
+    open: true,
+    // this sets a default port to 3000
+    port: 3000
+  }
 });
