da4da504b59a1726f3e24343eab43197b0740a31
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-07-10 19:43:56 +0900

    Add BoardsController

---
+++ CgvMate.Api/Controllers/BoardsController.cs
@@ -0,0 +1,37 @@
+﻿using CgvMate.Api.DTOs;
+using CgvMate.Api.Entities;
+using CgvMate.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CgvMate.Api.Controllers;
+
+[Route("board")]
+[ApiController]
+public class BoardsController : ControllerBase
+{
+    private readonly IBoardService _boardService;
+
+    public BoardsController(IBoardService boardService)
+    {
+        _boardService = boardService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllBoards()
+    {
+        var boards = await _boardService.GetAllBoardsAsync();
+        var boardDtos = boards.Select(BoardResDTO.FromEntity);
+        return Ok(boardDtos);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetBoardById(int id)
+    {
+        var board = await _boardService.GetBoardByIdAsync(id);
+        if (board == null)
+        {
+            return NotFound();
+        }
+        return Ok(BoardResDTO.FromEntity(board));
+    }
+}
---
+++ CgvMate.Api/DTOs/BoardResDTO.cs
@@ -0,0 +1,20 @@
+﻿using CgvMate.Api.Entities;
+
+namespace CgvMate.Api.DTOs;
+
+public class BoardResDTO
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+
+    public static BoardResDTO FromEntity(Board board)
+    {
+        return new BoardResDTO
+        {
+            Id = board.Id,
+            Title = board.Title,
+            Description = board.Description,
+        };
+    }
+}
