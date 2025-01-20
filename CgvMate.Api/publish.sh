51a857004bb6b69f55db42f514cd31c198a02799
woorim02 <99327851+woorim02@users.noreply.github.com>
2025-01-20 14:35:19 +0900

    v1.5.12

--- CgvMate.Api/publish.sh
+++ CgvMate.Api/publish.sh
@@ -1,1 +1,1 @@
-dotnet publish --os linux --arch x64 /t:PublishContainer -c Release -p:ContainerImageTag=v1.5.11
+dotnet publish --os linux --arch x64 /t:PublishContainer -c Release -p:ContainerImageTag=v1.5.12
